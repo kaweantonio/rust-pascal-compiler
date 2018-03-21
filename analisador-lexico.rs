f7e7d5045315b5622445e55dad0585e6b5f024bb
Kawe Antônio <kaweantonio@gmail.com>
2018-03-21 13:52:46 -0300

    Mudanças

--- analisador-lexico.rs
+++ analisador-lexico.rs
@@ -15,20 +15,41 @@ fn cria_tabela_de_simbolos() -> Vec<String>{
 	return vec;
 }
 
-// abre arquivo contendo o programa-fonte em código-fonte Pascal
-fn abre_arquivo(filename: &str){
-	let st = cria_tabela_de_simbolos();
+fn match_char(data: &char) -> bool {
+    match *data {
+        '\x01'...'\x08' |
+        '\u{10FFFE}'...'\u{10FFFF}' => true,
+        _ => false,
+    }
+}
 
+// abre arquivo contendo o programa-fonte em código-fonte Pascal
+fn abre_arquivo(filename: &str){ 
 	let mut f = File::open(filename).expect("file not found");
 
   let mut contents = String::new();
-  f.read_to_string(&mut contents)
-      .expect("something went wrong reading the file");
 
+  f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
-  let v : Vec<String>;
-  v = contents.split_whitespace().map(String::from).collect();
-  println!("{:?}", v);
+  let v: Vec<String> = contents.split_whitespace().map(String::from).collect();
+
+  let mut identificadores = cria_tabela_de_simbolos();
+  let mut significados = cria_tabela_de_simbolos();
+
+ 	// for i in 0..v.len(){
+ 	// 	let t = v[i].len();
+
+ 	// 	for j in 0..t {
+ 	// 		if (v[i].chars().next().unwrap() < '\x30')
+ 	// 	}
+ 	// }
+
+  println!("{}", '\u{10FFFE}');
+  // for i in 0..v.len(){
+  // 	st.push(v[i].to_string());
+  // }
+
+  // println!("{:?}", st);
 }
 
 fn main(){
